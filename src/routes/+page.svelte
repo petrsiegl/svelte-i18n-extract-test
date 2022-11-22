d727ac590e8518270513a838e278237757c8fca8
Petr Siegl <p.siegl@email.cz>
2022-11-22 10:58:37 +0100

    fix: init i18n

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import Test from "../lib/Test.svelte";
+    import {init} from 'svelte-i18n'
+
+    init({
+            fallbackLocale: 'en',
+        }
+    );
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Test/>
